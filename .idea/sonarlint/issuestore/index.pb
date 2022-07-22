7222137641a431e4949a587f41063fc421509013
ybgabamano <yves-boris.gaba-mano@ext.ingroupe.com>
2022-07-23 01:40:03 +0200

    Mise en place de la gestion des exceptions

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+p
+@gestiondestock/gestiondestock/src/main/resources/application.yml,6\e\6e4134d6254d2eacf2b86cafdd7853cc1b95add7
---
+++ gestiondestock/gestiondestock/src/main/java/rca/risbo/gestiondestock/exception/ErrorCodes.java
@@ -0,0 +1,36 @@
+package rca.risbo.gestiondestock.exception;
+
+/**
+ * @author ybgabamano
+ *  Gestion des codes erreurs personnalisées.
+ */
+public enum ErrorCodes {
+
+    ARTICLE_NOT_FOUND(1000),
+    ARTICLE_NOT_VALID(1001),
+    CATEGORY_NOT_FOUND(2000),
+    // TODO complete the rest of the Error code
+    CLIENT_NOT_FOUND(3000),
+    COMMANDE_CLIENT_NOT_FOUND(4000),
+    COMMANDE_FOURNISSEUR_NOT_FOUND(5000),
+    ENTREPRISE_NOT_FOUND(6000),
+    FOURNISSEUR_NOT_FOUND(7000),
+    LIGNE_COMMANDE_CLIENT_NOT_FOUND(8000),
+    LIGNE_COMMANDE_FOURNISSEUR__NOT_FOUND(9000),
+    LIGNE_VENTE_NOT_FOUND(10000),
+    MVT_STK_NOT_FOUND(11000),
+    UTILISATEUR_NOT_FOUND(120000),
+    VENTE_NOT_FOUND(13000)
+    ;
+
+    private int code;
+
+    ErrorCodes(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+}
